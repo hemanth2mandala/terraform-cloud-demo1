0a4a184d081747a5096764bee1941ca7c24229e0
hemanth2mandala <hemanthmandala458@gmail.com>
2023-06-28 23:19:53 +0530

    dev resource count increased to 2

--- c2-ec2instance.tf
+++ c2-ec2instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app-splunk" {
   instance_type          = var.aws_ec2_type
   key_name               = "mandalah"
   user_data              = file("apache-install.sh")
-  count                  = 1
+  count                  = 2
   vpc_security_group_ids = [aws_security_group.app-sg.id]
 
 
