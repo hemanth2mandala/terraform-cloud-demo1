12db8e58ec0048fe68a8d3b0d065530eb9c54de6
hemanth2mandala <hemanthmandala458@gmail.com>
2023-08-16 21:15:38 +0530

    changed apache file to original version

--- apache-install.sh
+++ apache-install.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo service httpd start
 sudo systemctl enable httpd
-echo "<h1>Hello my friend</h1>" > /var/www/html/index.html
+echo "<h1>Hello My friend</h1>" > /var/www/html/index.html
